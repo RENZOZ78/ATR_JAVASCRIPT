c800ad11b1ea2d76d3245d730c47950006a457a1
RENZOZ78 <didierbap@gmail.com>
2022-02-09 14:37:54 +0100

    exo jquerry1

---
+++ jquerry/Exercice1/scroll.js
@@ -0,0 +1,14 @@
+$(function(){
+    //traitement
+    //modifier le contenu du li sruvele
+    //this= seulement pour la balsie span du li qui est survolée
+    $("li").hover(function(){
+        $("span",this).html('-');
+        $("p",this).show();
+
+
+    },function(){
+        $("span",this).html('+');
+        $("p",this).hide();
+    });
+})
